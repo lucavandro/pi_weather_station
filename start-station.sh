53a51ab17c12a903529765f6735ecabd97104303
Valentina <lucavandro@gmail.com>
2017-05-22 23:11:02 +0200

    bug fix

--- start-station.sh
+++ start-station.sh
@@ -4,4 +4,10 @@
 # http://www.raspberrypi-spy.co.uk/2014/05/how-to-autostart-apps-in-rasbian-lxde-desktop/
 
 echo "Starting Weather Station"
+
+if [ ! -d "/home/pi/pi_weather_station/pictures" ]; then
+  # Control will enter here if $DIRECTORY doesn't exist.
+  mkdir /home/pi/pi_weather_station/pictures
+fi
+
 /usr/bin/python /home/pi/pi_weather_station/weather_station.py
